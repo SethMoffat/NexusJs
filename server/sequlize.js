56905ee97d50c06900c982971581853e2161cc44
Seth Moffat <88804196+SethMoffat@users.noreply.github.com>
2022-04-20 18:20:49 -0600

    Update sequlize.js

--- server/sequlize.js
+++ server/sequlize.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 const sequelize = new Sequelize(
-    "postgres://fjnqzpxobjnikc:ae54f99ad42e2f3dfb0d9f1e9d394d8e9699eb1383acd527d343fad6733240a9@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d6d3vsn6jedm4h",
+    "put link here"
     {
         dialect: "postgres",
         dialectOptions: {
